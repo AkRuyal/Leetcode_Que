4a8e8e4e2a7064d68d5c4f1d2452b30784874f49
AkRuyal <shinigamiwithl@gmail.com>
2026-09-02 20:53:39 +0530

    Time: 1 ms (99.79%) | Memory: 47.5 MB (21.27%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int j = 0;
+        int f = 0;
+        int i = 0;
+        int c = 0;
+        //chatgpt apporach
+        while (i < n - 1) {
+
+            if (i + nums[i] > f) {
+                f = i + nums[i];
+            }
+
+            if (i == c) {
+                j++;
+                c = f;
+            }
+
+            i++;
+        }
+
+        return j;
+        // my approach
+        // while(i<n){
+        //     a = (n-1) - nums[i];
+        //     if(a>0){
+        //         while(k<nums[i]){
+        //             if(nums[k]>f){
+        //                 if(f<n-1){
+        //                     f = nums[k];
+        //                 }
+                        
+        //             }
+        //         }
+        //         i += f;
+        //         j++;
+        //     }else{
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // return j-1;
+    }
+}
